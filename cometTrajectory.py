fa2eb45c293f66f8a81cb4b0c710e04ecc5715b3
KyleMcGregorOnline <112841262+KyleMcGregorOnline@users.noreply.github.com>
2026-03-12 15:02:33 +1100

    Added planets orbits during comets entry through solar system

--- cometTrajectory.py
+++ cometTrajectory.py
@@ -3,10 +3,24 @@ from astropy.time import Time
 import matplotlib.pyplot as plt
 
 cometId = '3I' # Comet 3/I ATLAS
-cometObserverLocation = '500@0' # barycenter
-cometTimeSpec = {'start':'2025-07-01', 'stop':'2026-02-01', 'step':'1d'} # Starting from comets entry to approximate exit with step of 1 day.
+planets = { 'Mercury': '199', 'Venus': '299', 'Earth': '399', 'Mars': '499', 'Jupiter': '599', 'Saturn': '699', 'Uranus': '799', 'Neptune': '899'
+} # Planets Mercury to Neptune
 
-comet = Horizons(id = cometId, location = cometObserverLocation, epochs = cometTimeSpec)
+barycenter = '500@0'
+timespec = {'start':'2025-01-01', 'stop':'2027-01-01', 'step':'1d'} # Rough dates of comets travel through solar system
+
+
+
+# Retrieve comet positions
+comet = Horizons(id = cometId, location = barycenter, epochs = timespec)
+cometVec = comet.vectors()
+
+# Retrieve planet positions
+planetVecs = {}
+for name, id in planets.items():
+    planet = Horizons(id = id, location = barycenter, epochs = timespec)
+    planetVecs[name] = planet.vectors()
+
 
 # Print ephemerides
 '''
@@ -24,10 +38,16 @@ plt.title('3/I ATLAS Path across sky')
 plt.show()
 '''
 
-vec = comet.vectors()
-
+# Plots solar system and comet traj
 plt.figure()
-plt.plot(vec['x'], vec['y'])
+
+# Comet trajectory
+plt.plot(cometVec['x'], cometVec['y'])
+
+# Planet orbits
+for name, planetVec in planetVecs.items():
+    plt.plot(planetVec['x'], planetVec['y'], label = name)
+    
 plt.scatter(0, 0)  
 
 plt.xlabel("x")
@@ -35,7 +55,9 @@ plt.ylabel("y")
 plt.title("3/I ATLAS Orbit around sun")
 plt.text(0,0, "   Solar System Barycenter")
 
-plt.xlim(-5, 5)
-plt.ylim(-5,5)
+plt.xlim(-30, 30)
+plt.ylim(-30, 30)
+
+plt.legend()
 
 plt.show()
